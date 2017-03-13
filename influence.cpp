774c9f6342992d8e88320a3f882a6bae6a7c44dd
HibaD <hiba_durvesh@hotmail.com>
2017-03-13 12:14:42 -0400

    Can find both influencers

--- influence.cpp
+++ influence.cpp
@@ -10,71 +10,115 @@
 
 using namespace std;
 
-class Graph{
+int TI1;
+class Graph {
 private:
     int V;
     list< pair <int, double> > *adj;
-    
+    bool influenced [10000];
+
 public:
     Graph(int V);
     void addEdge(int u, int v, double w);
     int shortestPath(int s, int T);
+    int shortestPath2(int s, int T);
 };
 
-Graph::Graph(int V){
+Graph::Graph(int V) {
     this->V = V;
     adj = new list< pair <int, double> > [V];
+    for (int i = 0; i < V; i++) {
+        influenced[i] = false;
+    }
 }
 
 int count = 0;
 
-void Graph::addEdge(int u, int v, double w){
+void Graph::addEdge(int u, int v, double w) {
     adj[u].push_back(make_pair(v, w));
     //adj[v].push_back(make_pair(w, u));
 }
 
 typedef pair<double, int> iPair;
 
-int Graph::shortestPath(int src, int T)
-{
-    priority_queue< iPair, vector <iPair> , greater<iPair> > pq;
-    
+int Graph::shortestPath(int src, int T) {
+    priority_queue< iPair, vector <iPair>, greater<iPair> > pq;
+
     vector<double> dist(V, INT_MAX);
- 
+
     pq.push(make_pair(0, src));
     dist[src] = 0;
- 
-    while (!pq.empty())
-    {
+
+    while (!pq.empty()) {
         int u = pq.top().second;
         pq.pop();
- 
+
         list< pair <int, double> >::iterator i;
-        for (i = adj[u].begin(); i != adj[u].end(); ++i)
-        {
+        for (i = adj[u].begin(); i != adj[u].end(); ++i) {
             int v = (*i).first;
             double weight = (*i).second;
- 
-            if (dist[v] > dist[u] + weight)
-            {
+
+            if (dist[v] > dist[u] + weight) {
                 dist[v] = dist[u] + weight;
                 pq.push(make_pair(dist[v], v));
             }
         }
     }
-    
-    printf("Vertex Distance from Source\n");
     int spread = 0;
-    for (int i = 0; i < V; ++i){
-        printf("%d     %f\n", i, dist[i]);
-        if (dist[i] <= T){
+    for (int i = 0; i < V; ++i) {
+        if (dist[i] <= T) {
             spread++;
         }
     }
     return spread;
 }
 
-struct Influencers{
+int Graph::shortestPath2(int src, int T) {
+    priority_queue< iPair, vector <iPair>, greater<iPair> > pq;
+
+    vector<double> dist(V, INT_MAX);
+
+    pq.push(make_pair(0, src));
+    dist[src] = 0;
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        list< pair <int, double> >::iterator i;
+        for (i = adj[u].begin(); i != adj[u].end(); ++i) {
+            int v = (*i).first;
+            double weight = (*i).second;
+
+            if (dist[v] > dist[u] + weight) {
+                dist[v] = dist[u] + weight;
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+
+    if (src == TI1){
+    for (int i = 0; i < V; i++) {
+        if (dist[i] <= T) {
+            influenced[i] = true;
+            cout << "Node " << i << " was influenced \n";
+        }
+    }
+    }
+    
+   // printf("Marginal Vertex Distance from Source\n");
+    int spread = 0;
+    for (int i = 0; i < V; i++) {
+        //printf("%d     %f\n", i, dist[i]);
+        if (dist[i] <= T && influenced[i] == false && src != i) {
+            cout <<"I was here \n";
+            spread++;
+        }
+    }
+    return spread;
+}
+
+struct Influencers {
     int nodeA;
     int nodeB;
     double weight;
@@ -83,80 +127,99 @@ struct Influencers{
 int numVertices = 0;
 
 // check if parser works
-void print(vector<Influencers> array){
-  for (int i=0; i<array.size(); i++){
-      cout << array[i].nodeA << " ";
-      cout << array[i].nodeB << " ";
-      cout << array[i].weight << " ";
-      cout << "\n";
-      if (array[i].nodeA > numVertices){
-          numVertices = array[i].nodeA;
-      }
-      if (array[i].nodeB > numVertices){
-          numVertices = array[i].nodeB;
-      }
-  }
+
+void print(vector<Influencers> array) {
+    for (int i = 0; i < array.size(); i++) {
+        /*cout << array[i].nodeA << " ";
+        cout << array[i].nodeB << " ";
+        cout << array[i].weight << " ";
+        cout << "\n";*/
+        if (array[i].nodeA > numVertices) {
+            numVertices = array[i].nodeA;
+        }
+        if (array[i].nodeB > numVertices) {
+            numVertices = array[i].nodeB;
+        }
+    }
 }
 
-Influencers lineToObject( string line){
-  Influencers newInfluencer;
-  istringstream ss(line);
-  //store values into the structure
-  ss >> newInfluencer.nodeA;
-  ss >> newInfluencer.nodeB;
-  ss >> newInfluencer.weight;
-  
-  return newInfluencer;
+Influencers lineToObject(string line) {
+    Influencers newInfluencer;
+    istringstream ss(line);
+    //store values into the structure
+    ss >> newInfluencer.nodeA;
+    ss >> newInfluencer.nodeB;
+    ss >> newInfluencer.weight;
+
+    return newInfluencer;
 }
 
-vector<Influencers> parser(char const *filename[]){
-  //Open input file produced by command line argument
-  ifstream file;
-  file.open(filename[1], ifstream::in);
-  //Initialize new vector of objects
-  vector<Influencers> array;
-  //read one line at a time
-  string line;
-  while(getline(file,line,'\n')){
-    array.push_back(lineToObject(line));
-  }
-  file.close();
-  return array;
-}
-
-void TopInfluencers (Graph g, int T){
-    
-    cout << "TOP-1 INFLUENCER: " << ", SPREAD: " << "TIME: " << "\n";
-    cout << "TOP-2 INFLUENCER: " << ", MARGINAL SPREAD: " << "TIME: " << "\n";
+vector<Influencers> parser(char const *filename[]) {
+    //Open input file produced by command line argument
+    ifstream file;
+    file.open(filename[1], ifstream::in);
+    //Initialize new vector of objects
+    vector<Influencers> array;
+    //read one line at a time
+    string line;
+    while (getline(file, line, '\n')) {
+        array.push_back(lineToObject(line));
+    }
+    file.close();
+    return array;
 }
 
 int main(int argc, char const *argv[]) {
-  vector<Influencers> array = parser(argv);
-  print(array);
-  
-  Graph g(numVertices);
-  for (int i=0; i < array.size(); i++){
-      g.addEdge(array[i].nodeA, array[i].nodeB, array[i].weight);
-  }
-  
-  int T = atoi(argv[2]);
-  
-  int TI1, temp, spread, spreadTI1 = 0;
-  temp = 0;
-  for (int i = 0; i < numVertices-1; i++){
-      spread = g.shortestPath(i, T);
-      if (spread > spreadTI1){
-          TI1 = i;
-          spreadTI1 = spread;
-      }
-  }
-  
-  cout << "TOP-1 INFLUENCER: " << TI1 << ", SPREAD: " << spreadTI1 << " TIME: " << "\n";
+    vector<Influencers> array = parser(argv);
+    print(array);
+
+    Graph g(numVertices);
+    for (int i = 0; i < array.size(); i++) {
+        g.addEdge(array[i].nodeA, array[i].nodeB, array[i].weight);
+    }
+
+    int T = atoi(argv[2]);
+
+    int temp, spread, spreadTI1 = 0;
+    temp = 0;
+    for (int i = 0; i < numVertices - 1; i++) {
+        spread = g.shortestPath(i, T);
+        if (spread > spreadTI1) {
+            TI1 = i;
+            spreadTI1 = spread;
+        }
+    }
+
+    int TI2, temp2, spread2, spreadTI2 = 0;
+    temp2 = 0;
+    cout << "node " << TI1 << "\n";
+    spread2 = g.shortestPath2(TI1, T);
     
-  //g.shortestPath(0, T);
-  
-  //TopInfluencers(g, T);
-  //print(array);
-  return 0;
+    cout << "Vertex: " << TI1 << " Marginal Spread: " << spread2 << "\n";
+    for (int i = 0; i < TI1; i++) {
+        cout << "node " << i << "\n";
+        spread2 = g.shortestPath2(i, T);
+        cout << "Vertex: " << i << " Marginal Spread: " << spread2 << "\n";
+        if (spread2 > spreadTI2) {
+            TI2 = i;
+            spreadTI2 = spread2;
+        }
+    }
+
+    for (int i = TI1 + 1; i < numVertices - 1; i++) {
+        spread2 = g.shortestPath2(i, T);
+        cout << "node " << i << "\n";
+        cout << "Vertex: " << i << " Marginal Spread: " << spread2 << "\n";
+        if (spread2 > spreadTI2) {
+            TI2 = i;
+            spreadTI2 = spread2;
+        }
+    }
+
+
+    cout << "TOP-1 INFLUENCER: " << TI1 << ", SPREAD: " << spreadTI1 << " TIME: " << "\n";
+    cout << "TOP-2 INFLUENCER: " << TI2 << ", MARGINAL SPREAD: " << spreadTI2 << " TIME: " << "\n";
+
+    return 0;
 }
 
